64d2d5ae3a06070e84ca3232b7e4415ea5710c30
soyoung <bgd02089@hanmail.net>
2018-10-14 03:47:35 +0900

    다운로드 기능 추가는 했지만 아직 실행 안됨

--- app/src/main/java/an_firebase/example/com/an_firebase/MainActivity.java
+++ app/src/main/java/an_firebase/example/com/an_firebase/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -11,16 +12,24 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -29,11 +38,23 @@ public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
 
+    private TextView tvFileName;
+    private ImageView imageView;
+    private EditText edtFileName;
+
     private Button btChoose;
     private Button btUpload;
+    private Button btDownload;
+    private Button btCheck;
     private ImageView ivPreview;
 
+    private Uri fileUri;
+    private Bitmap bitmap;
+    private StorageReference imageReference;
+    private StorageReference fileRef;
+
     private Uri filePath;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +63,16 @@ public class MainActivity extends Activity {
 
         btChoose = (Button) findViewById(R.id.bt_choose);
         btUpload = (Button) findViewById(R.id.bt_upload);
+        btDownload = (Button) findViewById(R.id.bt_download);
+        btCheck = (Button) findViewById(R.id.bt_check);
         ivPreview = (ImageView) findViewById(R.id.iv_preview);
 
+        imageReference = FirebaseStorage.getInstance().getReference().child("images");
+        fileRef = null;
+        progressDialog = new ProgressDialog(this);
+
         //버튼 클릭 이벤트
+
         btChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,6 +84,12 @@ public class MainActivity extends Activity {
             }
         });
 
+        btCheck.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //업로드
+                sendSignInLinkToEmail();
+            }
+        });
         btUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,7 +97,13 @@ public class MainActivity extends Activity {
                 uploadFile();
             }
         });
-
+        btDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //업로드
+                downloadFile();
+            }
+        });
     }
 
     //결과 처리
@@ -71,7 +111,7 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         //request코드가 0이고 OK를 선택했고 data에 뭔가가 들어 있다면
-        if(requestCode == 0 && resultCode == RESULT_OK){
+        if (requestCode == 0 && resultCode == RESULT_OK) {
             filePath = data.getData();
             Log.d(TAG, "uri:" + String.valueOf(filePath));
             try {
@@ -84,6 +124,37 @@ public class MainActivity extends Activity {
         }
     }
 
+    private void sendSignInLinkToEmail()
+    {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        Intent intent = getIntent();
+        String emailLink = intent.getData().toString();
+
+    // Confirm the link is a sign-in with email link.
+        if (auth.isSignInWithEmailLink(emailLink)) {
+            // Retrieve this from wherever you stored it
+            String email = "bgd02089@gmail.com";
+
+            // The client SDK will parse the code from the link for you.
+            auth.signInWithEmailLink(email, emailLink)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "Successfully signed in with email link!");
+                                AuthResult result = task.getResult();
+                                // You can access the new user via result.getUser()
+                                // Additional user info profile *not* available via:
+                                // result.getAdditionalUserInfo().getProfile() == null
+                                // You can check if the user is new or existing:
+                                // result.getAdditionalUserInfo().isNewUser()
+                            } else {
+                                Log.e(TAG, "Error signing in with email link", task.getException());
+                            }
+                        }
+                    });
+        }
+    }
     //upload the file
     private void uploadFile() {
         //업로드할 파일이 있으면 수행
@@ -103,6 +174,7 @@ public class MainActivity extends Activity {
             //storage 주소와 폴더 파일명을 지정해 준다.
             StorageReference storageRef = storage.getReferenceFromUrl("gs://an-firebase.appspot.com").child("images/" + filename);
             //올라가거라...
+
             storageRef.putFile(filePath)
                     //성공시
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -125,7 +197,7 @@ public class MainActivity extends Activity {
                         @Override
                         public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
                             @SuppressWarnings("VisibleForTests") //이걸 넣어 줘야 아랫줄에 에러가 사라진다. 넌 누구냐?
-                                    double progress = (100 * taskSnapshot.getBytesTransferred()) /  taskSnapshot.getTotalByteCount();
+                                    double progress = (100 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
                             //dialog에 진행률을 퍼센트로 출력해 준다
                             progressDialog.setMessage("Uploaded " + ((int) progress) + "% ...");
                         }
@@ -135,4 +207,55 @@ public class MainActivity extends Activity {
         }
     }
 
+    StorageReference mStorageRef;  //mStorageRef was previously used to transfer data.
+
+    private void downloadFile() {
+        if (filePath != null) {
+            progressDialog.setTitle("Downloading...");
+            progressDialog.setMessage(null);
+            progressDialog.show();
+
+            final long ONE_MEGABYTE = 1024 * 1024;
+            try {
+                final File localFile = File.createTempFile("images", "jpg");
+
+                mStorageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        Bitmap bmp = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                        ivPreview.setImageBitmap(bmp);
+                        progressDialog.dismiss();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }).addOnProgressListener(new OnProgressListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        // progress percentage
+                        double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+
+                        // percentage in progress dialog
+                        progressDialog.setMessage("Downloaded " + ((int) progress) + "%...");
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "Upload file before downloading", Toast.LENGTH_LONG).show();
+        }
+
+       /* private boolean validateInputFileName (String fileName){
+            if (TextUtils.isEmpty(fileName)) {
+                Toast.makeText(MainActivity.this, "Enter file name!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            return true;
+            }
+        */
+    }
 }
